fe3ca21b4158b18e5c78a47a5064d3045f2b2da1
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2022-07-22 15:38:35 +0100

    entities created

---
+++ Backend/src/main/java/com/accella/crud/entities/PersonAddress.java
@@ -0,0 +1,72 @@
+package com.accella.crud.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class PersonAddress implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String street;
+	private String city;
+	private String state;
+	private int postalCode;
+
+	public PersonAddress() {
+	}
+
+	public PersonAddress(Long id, String street, String city, String state, int postalCode) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.postalCode = postalCode;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+
+}
